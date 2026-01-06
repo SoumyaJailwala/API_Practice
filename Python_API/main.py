ccbb5f2eddaf2b32641ee3391bf9b8754457ddad
Soumya Jailwala <SoumyaJailwala@users.noreply.github.com>
2026-01-06 13:22:46 -0500

    setup

---
+++ Python_API/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Create Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travel.db'
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
